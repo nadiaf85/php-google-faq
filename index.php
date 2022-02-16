463e1e9ba470035821a3e51e308f97e769a52eb1
nadiaf85 <nadia.fiorio@alice.it>
2022-02-16 16:20:55 +0100

    creata repo

---
+++ index.php
@@ -0,0 +1,52 @@
+<!-- Riscrivere questa pagina del sito google
+https://policies.google.com/faq. -->
+
+
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$faq = [
+    [
+        "domanda" => "<h2> Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?  </h2>",
+        "risposta" => 
+    ],
+    [
+        "domanda" => "<h2> Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro? </h2>",
+        "risposta" => 
+    ],
+    [
+        "domanda" => "<h2> Perché il mio account è associato a un paese? </h2>",
+        "risposta" => 
+    ],
+    [
+        "domanda" => "<h2> Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google? </h2>",
+        "risposta" => 
+    ],
+    [
+        "domanda" => "<h2> Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web? </h2>",
+        "risposta" => 
+    ],
+]
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+    
+?>
+    
+</body>
+</html>
